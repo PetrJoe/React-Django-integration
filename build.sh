6101dc9a70b3ec92abea2642a6c37c6b7b5f6432
“PetrJoe” <petrjoe01@gmail.com>
2023-12-15 00:50:38 +0100

    comment migrate and makemigration from build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
